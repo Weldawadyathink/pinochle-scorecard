4f4ae41ae41649fe5c1c2c40f0d034b50e02b4b2
Spenser Bushey <spenser@spenserbushey.com>
2024-03-30 21:35:20 -0700

    Round editor user interface

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,10 @@ import { PinochleRoundEditor } from "./components/PinochleRoundEditor";
 import { PinochleRound } from "./shared/PinochleRound";
 
 export default function App() {
-  const [data, setData] = useState(new PinochleRound());
+  const [data, setData] = useState(new PinochleRound({ roundComplete: true }));
 
   return (
-    <div className="container max-w-xl mx-auto p-6">
+    <div className="container max-w-2xl mx-auto p-6">
       <PinochleRoundEditor data={data} onChange={(d) => setData(d)} />
     </div>
   );
--- src/components/PinochleRoundEditor.tsx
+++ src/components/PinochleRoundEditor.tsx
@@ -1,5 +1,7 @@
 import { PinochleRound } from "../shared/PinochleRound.ts";
 import { set, cloneDeep } from "lodash-es";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
 
 export interface PinochleRoundEditorProps {
   data: PinochleRound;
@@ -10,26 +12,79 @@ export function PinochleRoundEditor({
   data,
   onChange,
 }: PinochleRoundEditorProps) {
-  function setData(path: string, value: unknown) {
+  function setData(path: string, value: any) {
     const temp = cloneDeep(data);
     set(temp, path, value);
     onChange(temp);
   }
+
+  function setInt(path: string, value: string) {
+    const temp = cloneDeep(data);
+    set(temp, path, parseInt(value));
+    onChange(temp);
+  }
+
   return (
     <>
-      <div className="columns-2">
+      <div className="flex flex-row text-xl gap-8">
         <div>
-          <p>
-            Team A Meld:{" "}
-            <input
-              className="max-w-12"
+          <div className="flex flex-row gap-4">
+            <Button
+              variant={data.teamWithBid === "a" ? "default" : "secondary"}
+              onClick={() => setData("teamWithBid", "a")}
+            >
+              Take Bid
+            </Button>
+            <Input
               type="number"
-              value={data.teamAMeldScore}
-              onChange={(e) => setData("teamAMeldScore", e.target.value)}
+              disabled={data.teamWithBid !== "a"}
+              value={data.bid}
+              onChange={(e: any) => setInt("bid", e.target.value)}
             />
-          </p>
+          </div>
+          <h1>Team A Meld</h1>
+          <Input
+            type="number"
+            value={data.teamAMeldScore}
+            onChange={(e: any) => setInt("teamAMeldScore", e.target.value)}
+          />
+          <h1>Team A Tricks</h1>
+          <Input
+            type="number"
+            value={data.teamATrickScore}
+            onChange={(e: any) => setInt("teamATrickScore", e.target.value)}
+          />
+          <h1>Total Score: {data.teamATotalScore}</h1>
         </div>
-        <p>{JSON.stringify(data)}</p>
+        <div>
+          <div className="flex flex-row gap-4">
+            <Button
+              variant={data.teamWithBid === "b" ? "default" : "secondary"}
+              onClick={() => setData("teamWithBid", "b")}
+            >
+              Take Bid
+            </Button>
+            <Input
+              type="number"
+              value={data.bid}
+              disabled={data.teamWithBid !== "b"}
+              onChange={(e: any) => setInt("bid", e.target.value)}
+            />
+          </div>
+          <h1>Team B Meld</h1>
+          <Input
+            type="number"
+            value={data.teamBMeldScore}
+            onChange={(e: any) => setInt("teamBMeldScore", e.target.value)}
+          />
+          <h1>Team B Tricks</h1>
+          <Input
+            type="number"
+            value={data.teamBTrickScore}
+            onChange={(e: any) => setInt("teamBTrickScore", e.target.value)}
+          />
+          <h1>Total Score: {data.teamBTotalScore}</h1>
+        </div>
       </div>
     </>
   );
